88d00ee294b441fd60efc684401f6effa6d16237
Anastasiia Nedbai <nedbayfomina@gmail.com>
2026-06-15 01:21:24 +0300

    перший модуль

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-print("Hello, World!")
-print("Welcome to the world of programming!")
-print("This is a simple Python program.")
+# print("Hello, World!")
+# print("Welcome to the world of programming!")
+# print("This is a simple Python program.")
 
-print("Let's do some basic arithmetic:")
-a = 10
-b = 5
-c = a + b   
-print(f"The sum of {a} and {b} is: {c}")
+# print("Let's do some basic arithmetic:")
+# a = input("Enter a number: ")
+# b = input("Enter another number: ")
+# a = int(a)
+# b = int(b)
+# c = a + b   
+# print(f"The sum of {a} and {b} is: {c}")
 
+# a = float(input("Введіть сторону квадрата a: "))
+# P = 4 * a
+# print(f"Периметр квадрата дорівнює {P}")
+
+# sentence_one = "Hello world"
+# sentence_two = "Hello " + "world"
+# # Відображення змінних
+# print(sentence_one, sentence_two, sep='\n')
+
+# # Перевірка рівності і ідентичності
+# print(f'Are sentence equal? {sentence_one == sentence_two}')
+# print(f'Are sentence identical? {sentence_one is sentence_two}')
+
+# # Перевірка місця збереження у памяті
+# print(f'Path of sentence in memory {id(sentence_one)}')
+# print(f'Path of sentence in memory {id(sentence_two)}')
+
+FLOORS = 30
+APARTMENTS_PER_FLOOR = 3
+
+apartment_number = int(input('Enter apartment number: '))
+apartments_per_entrance = FLOORS * APARTMENTS_PER_FLOOR
+entrance_number = (apartment_number - 1) // apartments_per_entrance + 1
+floor_number = ((apartment_number - 1) % apartments_per_entrance) // APARTMENTS_PER_FLOOR + 1
+print(f"Entrance number {entrance_number}, Floor number {floor_number}")
---
+++ unit.py
@@ -0,0 +1,24 @@
+# # Введення (отримання даних)
+# ім_я = input("Введіть ваше ім'я: ")
+
+# # Перетворення (обробка даних)
+# вітання = f"Привіт, {ім_я}!"
+
+# # Виведення (виведення даних)
+# print(вітання)
+
+# age = 15
+# is_adult = age >= 18  # True
+# print(f"Чи є людина повнолітньою? {is_adult}")
+
+# s1 = "Hello"
+# s2 = "world!"
+# joined_string = s1 + " " + s2
+# print(joined_string)
+
+# n = 5000
+
+# hours = n // (60 * 60)
+# minutes = (n - hours * 60 * 60) // 60
+# seconds = n - hours * 60 * 60 - minutes * 60
+# print(f"{n} секунд - це {hours} годин, {minutes} хвилин і {seconds} секунд.")
