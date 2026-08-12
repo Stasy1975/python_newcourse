e08d679c49a6984c2d0b4d11d96fe49a0fa43ec2
Anastasiia Nedbai <nedbayfomina@gmail.com>
2026-08-12 17:03:48 +0300

    add modul 3

---
+++ text.py
@@ -0,0 +1,11 @@
+one_line_text = "Textual data in Python is handled with str objects," \
+                " or strings. Strings are immutable sequences of Unicode" \
+                " code points. String literals are written in a variety " \
+                " of ways: single quotes, double quotes, triple quoted."
+print(one_line_text)
+
+print("Hello my little\rsister")
+
+elements = ['earth', 'air', 'fire', 'water']
+result = ', '.join(elements)
+print(result)  # Виведе: 'earth, air, fire, water'
